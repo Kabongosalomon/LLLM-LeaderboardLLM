8df08bf89b5fed0cd13cd052ad67964c9d0c39fe
TIB Devboxes <salomon@aims.ac.za>
2023-09-05 05:21:19 +0200

    update evaluate

--- llama_v2_sft_train.py
+++ llama_v2_sft_train.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+s#!/usr/bin/env python
 # coding: utf-8
 
 # Fine-Tune Llama2-7b on custom dataset
@@ -83,21 +83,21 @@ script_args.seq_length
 # script_args.output_dir = "./model_ckpt/tdms_all_template_v2"
 # script_args.run_name = "sft_llama2_tdms_all_Template_v2"
 
-script_args.dataset_name = "./data/LLLM_DOCTEAT_TDM_ALL_TEMPLATE/fold1"
-script_args.output_dir = "./model_ckpt/docteat_tdm_f1_all_template"
-script_args.run_name = "sft_llama2_docteat_tdm_f1_all_Template"
-script_args.seq_length = 1024
-script_args.per_device_train_batch_size = 6
-script_args.gradient_accumulation_steps = 2
-# # multi GPU
-# script_args.per_device_train_batch_size = 4
-
-# script_args.dataset_name = "./data/LLLM_LONG_TDM_ALL_TEMPLATE/fold1"
-# script_args.output_dir = "./model_ckpt/long_tdm_f1_all_template"
-# script_args.run_name = "sft_llama2_long_tdm_f1_all_Template"
-# script_args.seq_length = 2400
-# script_args.per_device_train_batch_size = 2
+# script_args.dataset_name = "./data/LLLM_DOCTEAT_TDM_ALL_TEMPLATE/fold1"
+# script_args.output_dir = "./model_ckpt/docteat_tdm_f1_all_template"
+# script_args.run_name = "sft_llama2_docteat_tdm_f1_all_Template"
+# script_args.seq_length = 1024
+# script_args.per_device_train_batch_size = 6
 # script_args.gradient_accumulation_steps = 2
+# # # multi GPU
+# # script_args.per_device_train_batch_size = 4
+
+script_args.dataset_name = "./data/LLLM_LONG_TDM_ALL_TEMPLATE/fold1"
+script_args.output_dir = "./model_ckpt/long_tdm_f1_all_template"
+script_args.run_name = "sft_llama2_long_tdm_f1_all_Template"
+script_args.seq_length = 2400
+script_args.per_device_train_batch_size = 2
+script_args.gradient_accumulation_steps = 2
 
 script_args.save_steps = 50
 script_args.logging_steps = 50
